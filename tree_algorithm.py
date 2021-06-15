6a61a6ca381f9bfbf284705a2db05679461a72c4
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-06-15 16:16:20 +0900

    still working

--- tree_algorithm.py
+++ tree_algorithm.py
@@ -1,7 +1,18 @@
 # %%
 from datatable import fread
 from IPython.display import display
+
 # %%
-df_orig = fread('./sct_relationship_codes.csv', na_strings=['NA','']).to_pandas()
-df_orig.head()
+
+
+
+
+
+
+
+
 # %%
+
+if __name__ == '__main__':
+    df_orig = fread('./sct_relationship_codes.csv', na_strings=['NA','']).to_pandas()
+    display(df_orig.head())
