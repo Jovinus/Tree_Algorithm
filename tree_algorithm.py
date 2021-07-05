24f6bbf655570c9ec5060168015efe3c6578cb32
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-07-05 16:48:45 +0900

    fix some bugs

--- tree_algorithm.py
+++ tree_algorithm.py
@@ -1,17 +1,9 @@
-# %%
+# %% Import packages to use
 from datatable import fread
 from IPython.display import display
 import networkx as nx
 
-# %%
-df_orig = fread('./sct_relationship_codes.csv', na_strings=['NA','']).to_pandas()
-display(df_orig.head())
-
-# %%
-
-DiG = nx.from_pandas_edgelist(df_orig, target='sourceId', source='destinationId', create_using=nx.DiGraph())
-
-#%%
+# %% Hierarchical Graph Class
 class hierarchical_graph():
     
     def __init__(self, data, parent, source):
@@ -25,7 +17,10 @@ class hierarchical_graph():
                                              target=self.parent, 
                                              source=self.source, 
                                              create_using=nx.DiGraph())
-    
+        
+        ## Print maximum level between higher node and lowest node
+        print(max(nx.single_source_shortest_path_length(self.graph, 138875005).values()))
+        
     def get_descendent(self, node, level):
         decendents = nx.descendants_at_distance(G=self.graph, 
                                                 source=node, 
@@ -47,19 +42,4 @@ if __name__ == '__main__':
     
     test = hierarchical_graph(data=df_orig, parent='destinationId', source='sourceId')
     test.get_descendent(node=138875005, level=1)
-    
-    display(df_orig.head())
-    
-# %%
-print(nx.descendants_at_distance(G=DiG, source=138875005, distance=1))
-# %%
-test = nx.single_source_shortest_path_length(DiG, 138875005)
-max(test.values())
-# %%
-df_orig[(df_orig['sourceId'].isin(nx.descendants_at_distance(G=DiG, source=138875005, distance=1))) & (df_orig['active'] == True)]
-# %%
-
-df_orig[(df_orig['sourceId'].isin(nx.descendants(G=DiG, source=267038008))) & (df_orig['active'] == True)]
-# %%
-max(nx.single_source_shortest_path_length(DiG, 138875005).values())
-# %%
+    test.get_descendent_all(node=138875005)
